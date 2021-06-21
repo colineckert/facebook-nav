aad7b651a8c3bf15292f73389ed1a86c5b1a3d05
colineckert <colineckert10@gmail.com>
2021-06-21 09:29:22 -0700

    Add svg icons and styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import './index.css';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
   return (
     <Navbar>
-      <li>X</li>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
     </Navbar>
-  )
+  );
 }
 
 function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">{ props.children }</ul>
+      <ul className="navbar-nav">{props.children}</ul>
     </nav>
-  )
+  );
 }
 
 function NavItem(props) {
   return (
-    <li>
+    <li className="nav-item">
       <a href="#" className="icon-button">
         {props.icon}
       </a>
     </li>
-  )
+  );
 }
 
 export default App;
-
