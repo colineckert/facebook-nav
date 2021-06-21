dcead720d2ff89144edd2dfd1dc2162d22a22425
colineckert <colineckert10@gmail.com>
2021-06-21 08:24:55 -0700

    Create nav item component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 
 function App() {
   return (
-    <Navbar></Navbar>
+    <Navbar>
+      <li>X</li>
+    </Navbar>
   )
 }
 
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">
-
-      </ul>
+      <ul className="navbar-nav">{ props.children }</ul>
     </nav>
+  )
+}
+
+function NavItem(props) {
+  return (
+    <li>
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
   )
 }
 
