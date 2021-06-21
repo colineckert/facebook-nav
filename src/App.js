ef1b28ee33649008b33705c0fc2aced350d7ee28
colineckert <colineckert10@gmail.com>
2021-06-21 09:53:07 -0700

    create sinlge level dropdown and install transition group

--- src/App.js
+++ src/App.js
@@ -16,8 +16,40 @@ function App() {
       <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>
+        
+        <DropdownMenu />
+
+      </NavItem>
+
     </Navbar>
   );
+}
+
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}>
+          Settings
+      </DropdownItem>
+    </div>
+  )
 }
 
 function Navbar(props) {
@@ -29,11 +61,16 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+
+  const [open, setOpen] = useState();
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
   );
 }
